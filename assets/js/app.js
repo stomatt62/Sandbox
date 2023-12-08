3e7f2f051bf3bcd3c6751a612a99b2513776e61c
Hochedé Mattéo <matteo.hochede62@gmail.com>
2023-12-08 16:12:54 +0100

    third commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,52 @@
 console.log("hello World saint jo");
 
-const prenom  = "mattéo";
+const prenom = "mattéo";
 let year2 = 2024; //number
 const dob = 2002;
 const isAdmin = false; //boolean
-const apprenants = ["mattéo","florian"] //tableau
-const sac = {livre: "bd de tintin" ,trousse: "1", skills:["bagarre", "game",] } //objet
+const apprenants = ["mattéo", "florian"]; //tableau
+const sac = {
+  livre: "bd de tintin",
+  trousse: "1",
+  skills: ["bagarre", "game"],
+}; //objet
 const disque = document.querySelector(".disque");
-const btnPause =document.getElementById("btnPause");
-const paragraphe =document.querySelector(".div1 p");
+const btnPause = document.getElementById("btnPause");
+const paragraphe = document.querySelector(".div1 p");
 
 setTimeout(() => {
-    paragraphe.style.backgroundColor = "red"
+  paragraphe.style.backgroundColor = "red";
 }, 1000);
 
-btnPause.addEventListener("click",()=>{
-    disque.classList.toggle("pause");
+btnPause.addEventListener("click", () => {
+  disque.classList.toggle("pause");
 
-    if (disque.classList.contains("pause")){
-        btnPause.textContent = "play";
-    }else{
-        btnPause.textContent = "Pause";
-    }
+  if (disque.classList.contains("pause")) {
+    btnPause.textContent = "play";
+  } else {
+    btnPause.textContent = "Pause";
+  }
 });
 
 console.log(disque);
 console.log(`bonjour ${prenom}`); //concaténation
 
-
-console.log(apprenants[1]); 
+console.log(apprenants[1]);
 console.log(sac.skills[0]);
 
-apprenants.forEach(
-    data => {
-        console.log(`bonjour ${data}`)
-    }
-)
+apprenants.forEach((data) => {
+  console.log(`bonjour ${data}`);
+});
 
-const calculeAge = ()=>{
-    const age = year2 - dob;
+const calculeAge = () => {
+  const age = year2 - dob;
 
-   /* if(age > 18){
+  /* if(age > 18){
         console.log("rentre bonhomme.")
     }else{
         console.log("dehors mauviette")
     }
     console.log(`je connais ton age : ${age} ans.`);*/
-    age > 18 ? console.log("rentre bonhomme.") : console.log("dehors mauviette");
-}
+  age > 18 ? console.log("rentre bonhomme.") : console.log("dehors mauviette");
+};
 calculeAge();
-
